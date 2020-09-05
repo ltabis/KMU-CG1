a2a734b134473e64face34aaaa56d967c47b5002
ltabis <tab.rysak70@gmail.com>
2020-09-05 20:39:07 +0900

    :arrow_up: triangle yay.

--- CG1/src/main.cpp
+++ CG1/src/main.cpp
@@ -28,6 +28,14 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+
+        glVertex2d(.5f, .5f);
+        glVertex2d(-.5f, .5f);
+        glVertex2d(-.5f, -.5f);
+
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
