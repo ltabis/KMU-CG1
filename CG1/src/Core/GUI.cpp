066dbdc11b309391c04775b7aa56f8aef1bf904a
ltabis <tab.rysak70@gmail.com>
2020-09-18 13:29:47 +0900

    :rotating_light: linted wierd vs indentation.

--- CG1/src/Core/GUI.cpp
+++ CG1/src/Core/GUI.cpp
@@ -5,10 +5,10 @@
 
 CG::GUI::GUI(GLFWwindow* window, CG::GUI::Style style)
 {
-    IMGUI_CHECKVERSION();
+	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 330");
+	ImGui_ImplOpenGL3_Init("#version 330");
 
 	if (style == Style::CLASSIC)
 		ImGui::StyleColorsClassic();
@@ -18,27 +18,27 @@ CG::GUI::GUI(GLFWwindow* window, CG::GUI::Style style)
 
 CG::GUI::~GUI()
 {
-    ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplOpenGL3_Shutdown();
 	ImGui::DestroyContext();
 }
 
 void CG::GUI::newFrame()
 {
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
 }
 
 void CG::GUI::drawUI()
 {
-    ImGui::Begin("Tools");
-    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	ImGui::Begin("Tools");
+	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
-    ImGui::End();
+	ImGui::End();
 }
 
 void CG::GUI::renderGUI()
 {
 	ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
