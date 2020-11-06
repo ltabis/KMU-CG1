94dec3828ce81277e7cb5c61fd6d0ce81fd510e3
ltabis <tab.rysak70@gmail.com>
2020-11-06 15:55:27 +0900

    :white_check_mark: added a test scene to try out the plane shape.

---
+++ CG1/src/Tests/TestShapeAbstraction.cpp
@@ -0,0 +1,54 @@
+#include "TestShapeAbstraction.hpp"
+
+CG::Test::TestShapeAbstraction::TestShapeAbstraction()
+	: _fov { 45 }
+{
+	_sloader = std::make_unique<ShaderLoader>();
+
+	_sloader->load("./res/shaders/square.shader");
+	_sloader->attach("triangle");
+	_sloader->attach("color");
+	_sloader->createExecutable();
+
+	_plane = std::make_unique<Plane>(
+		glm::vec3(0.f),
+		glm::vec3(0.f),
+		glm::vec3(1.f)
+	);
+}
+
+CG::Test::TestShapeAbstraction::~TestShapeAbstraction()
+{
+}
+
+void CG::Test::TestShapeAbstraction::onStart()
+{
+}
+
+void CG::Test::TestShapeAbstraction::onUpdate(float deltaTime)
+{
+	_renderer->pollEvents();
+}
+
+void CG::Test::TestShapeAbstraction::onRender()
+{
+	ImGui::Begin("Projection transformation");
+	// control over the projection matrix.
+	if (ImGui::SliderFloat("FOV", &_fov, 45, 120, "%.1f"))
+		_renderer->setFov(_fov);
+	ImGui::End();
+
+	glm::mat4 mvp = _renderer->projectionMatrix() * _renderer->viewMatrix() * glm::mat4(1.f);
+	_sloader->setUniform("u_mvp", mvp);
+	_renderer->draw(_plane->vao(), _plane->ibo(), *_sloader);
+}
+
+void CG::Test::TestShapeAbstraction::onStop()
+{
+}
+
+void CG::Test::TestShapeAbstraction::onReset()
+{
+	// reseting the translation uniform.
+	_sloader->setUniform("u_mvp", glm::mat4(1.f));
+}
---
+++ CG1/src/Tests/TestShapeAbstraction.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Framework/ATest.hpp"
+#include "DataStructures/MatrixStack.hpp"
+#include "WorldObjects/Plane.hpp"
+
+namespace CG {
+	namespace Test {
+		class TestShapeAbstraction : public ATest
+		{
+		public:
+			TestShapeAbstraction();
+			~TestShapeAbstraction();
+
+			void onStart() override;
+			void onUpdate(float deltaTime) override;
+			void onRender() override;
+			void onStop() override;
+			void onReset() override;
+
+		private:
+			float _fov;
+
+			std::unique_ptr<ShaderLoader> _sloader;
+			std::unique_ptr<Plane> _plane;
+		};
+	}
+}
